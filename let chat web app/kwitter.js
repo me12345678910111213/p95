213b5005cf03b643d9ffe22621624231ef36066a
me12345678910111213 <127686973+me12345678910111213@users.noreply.github.com>
2023-08-13 11:32:53 +0100

    Add files via upload

---
+++ let chat web app/kwitter.js
@@ -0,0 +1,9 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDQ5uxcm8EPQ3f-HdoVOaydK1FNb7CUW44",
+    authDomain: "c94a-ae74f.firebaseapp.com",
+    projectId: "c94a-ae74f",
+    storageBucket: "c94a-ae74f.appspot.com",
+    messagingSenderId: "84913970016",
+    appId: "1:84913970016:web:52a8c5c42cf6f989f49c05"
+  };
